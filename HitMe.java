2b7004c55abfe023d292d2aea2951005df2984ae
kritika20sharma <53707301+kritika20sharma@users.noreply.github.com>
2019-10-29 21:26:00 +0530

    Update HitMe.java
    i hae added subtr. , lets see it works or not

--- HitMe.java
+++ HitMe.java
@@ -1,14 +1,24 @@
 import java.io.*;
 
-class HitMe {
-  public static void main(String[] args) {
+class HitMe
+{
+  public static void main(String[] args) 
+  {
     int a = 10;
     int b = 20;
     int sum = add(a,b);
+    int subtract = sub(a,b);
     System.out.println(sum);
+    System.out.println(subtract);
   }
 
-  public static int add(int a, int b) {
+  public static int add(int a, int b) 
+  {
     return a+b;
   }
+  public static int sub(int a,int b)
+  {
+    return a-b;
+  }
 }
+    
