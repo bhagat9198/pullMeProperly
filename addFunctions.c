175c9a416a26167f81d5c09f398b8400b3cf6384
probe2k <kyash054@gmail.com>
2019-10-30 11:52:39 +0530

    Rename + Functions add to C

--- addFunctions.c
+++ addFunctions.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
 
 int add(int a, int b);
+int sub(int a, int b);
 
 int main() {
   int a = 10;
   int b = 20;
-  int sum = add(a,b);
+  int sum = add(a, b);
+  int subtract = sub(a, b);
   printf("%d\n",sum);
+}
+
+int sub(int a, int b) {
+	return (a - b);
 }
 
  int add(int a, int b) {
