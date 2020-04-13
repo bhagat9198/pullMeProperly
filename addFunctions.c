440da1f62742df515d80d969fde54820c8282ca8
Rakshitha Mansi <57148790+mansi2497@users.noreply.github.com>
2020-04-13 13:06:56 +0530

    "added a printf statement , changed a variable name from subtract to dif and removed a variable result and gave a direct return statement."

--- addFunctions.c
+++ addFunctions.c
@@ -3,19 +3,22 @@
 int add(int a, int b);
 int sub(int a, int b);
 
-int main() {
+int main() 
+{
   int a = 10;
   int b = 20;
   int sum = add(a, b);
-  int subtract = sub(a, b);
+  int dif = sub(a, b);
   printf("%d\n",sum);
+ printf("%d\n",dif);
 }
 
-int sub(int a, int b) {
-	return (a - b);
+int sub(int a, int b)
+ {
+   return (a - b);
 }
 
- int add(int a, int b) {
-  int result = a + b;
-  return result;
+ int add(int a, int b) 
+{
+    return (a+b);
 }
