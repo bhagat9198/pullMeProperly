4b9148b56a40e45da173c97cd39c45377239c39a
Rituparna Warwatkar <rituparnaw16@gmail.com>
2021-08-24 22:17:51 +0530

    added gitignore file and added add and subtract function in C++

--- HitMe.java
+++ HitMe.java
@@ -2,19 +2,19 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-
 public class HitMe {
   public static void main(String[] args) {
     try {
-	    BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-	    System.out.println("Enter the operands : ");
-	    int a = Integer.parseInt(input.readLine());
-	    int b = Integer.parseInt(input.readLine());
-	    int sum = add(a, b);
-	    int subtract = sub(a, b);
-	    System.out.println(sum);
+      BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+      System.out.println("Enter the operands : ");
+      int a = Integer.parseInt(input.readLine());
+      int b = Integer.parseInt(input.readLine());
+      int sum = add(a, b);
+      int subtract = sub(a, b);
+      System.out.println(sum);
+      System.out.println(subtract);
     } catch (IOException exc) {
-    	exc.printStackTrace();
+      exc.printStackTrace();
     }
   }
 
@@ -23,7 +23,6 @@ public class HitMe {
   }
 
   public static int sub(int a, int b) {
-	return (a - b);
+    return (a - b);
   }
 }
-
--- codetech.cpp
+++ codetech.cpp
@@ -1,1 +1,22 @@
-#include<helloworld.>
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
+
+int main()
+{
+    int a = 0, b = 0;
+    cout << "Enter numbers a and b" << endl;
+    cin >> a >> b;
+    cout << "The addition is " << add(a, b) << endl;
+    cout << "The subtraction is " << subtract(a, b) << endl;
+    return 0;
+}
