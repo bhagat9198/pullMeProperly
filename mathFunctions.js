34acdf3b602b5f3013e376b611c545b818ca5801
Rituparna Warwatkar <rituparnaw16@gmail.com>
2021-08-24 22:27:31 +0530

    added javaScript functions for add and subtract

---
+++ mathFunctions.js
@@ -0,0 +1,16 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const a = 20;
+const b = 10;
+
+const sum = add(a, b);
+const diff = subtract(a, b);
+
+console.log(sum);
+console.log(diff);
